2cb355ed2d890e3635dc81afe203af06d18ccdb0
Tomas Breuer <tbreuer@acqua.net.ar>
2026-01-02 17:31:30 -0300

    Compatibilidad ELK stack

--- CE_Microservice/docker_maven/entypoints/maven-start.sh
+++ CE_Microservice/docker_maven/entypoints/maven-start.sh
@@ -19,7 +19,15 @@ else
   echo "El artefacto no se encontró en ${ARTIFACT_PATH}"
 fi
 echo 'Downloading Applications...'
-echo 'mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=${ATINA_REPOSITORY_PROTOCOL}://${ATINA_REPOSITORY_URL} -Dartifact=${MULEAPP_GROUP_ID}:${MULEAPP_ARTIFACT_ID}:${MULEAPP_VERSION} -Ddest=/home/apps/${MULEAPP_ARTIFACT_ID}-${MULEAPP_VERSION}-mule-application.jar'
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=${ATINA_REPOSITORY_PROTOCOL}://${ATINA_REPOSITORY_URL} -Dartifact=${MULEAPP_GROUP_ID}:${MULEAPP_ARTIFACT_ID}:${MULEAPP_VERSION} -Ddest=/home/apps/${MULEAPP_ARTIFACT_ID}-${MULEAPP_VERSION}-mule-application.jar -U
+echo 'mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=${ATINA_REPOSITORY_PROTOCOL}://${ATINA_REPOSITORY_URL} -Dartifact=${MULEAPP_GROUP_ID}:${MULEAPP_ARTIFACT_ID}:${MULEAPP_VERSION}'
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=${ATINA_REPOSITORY_PROTOCOL}://${ATINA_REPOSITORY_URL} -Dartifact=${MULEAPP_GROUP_ID}:${MULEAPP_ARTIFACT_ID}:${MULEAPP_VERSION} -U
 echo '------------------------------------------------------------------------------------'
-echo 'Maven Ejecutado'
+echo 'Copying application to Worker 1...'
+cp /home/repository/${GROUP_PATH}/${MULEAPP_ARTIFACT_ID}/${MULEAPP_VERSION}/${MULEAPP_ARTIFACT_ID}-${MULEAPP_VERSION}-mule-application.jar /home/apps/worker1/
+echo 'Worker 1 deployment complete.'
+echo '------------------------------------------------------------------------------------'
+echo 'Copying application to Worker 2...'
+cp /home/repository/${GROUP_PATH}/${MULEAPP_ARTIFACT_ID}/${MULEAPP_VERSION}/${MULEAPP_ARTIFACT_ID}-${MULEAPP_VERSION}-mule-application.jar /home/apps/worker2/
+echo 'Worker 2 deployment complete.'
+echo '------------------------------------------------------------------------------------'
+echo 'Maven Ejecutado - Both workers deployed'
